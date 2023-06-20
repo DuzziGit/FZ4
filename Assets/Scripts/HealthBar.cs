29b266e2f72ea14128e305f59d91aa8c9ee0c4ec
DuzziGit <danteduzzi@live.ca>
2023-06-20 00:46:31 -0400

    Enemy health bar is now only visible when damaged

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,16 +4,24 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Canvas healthBarCanvas; // Add this line to get a reference to the Canvas
 
     public void SetMaxHealth(int maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.minValue = 0; // Set the minimum value to 0
         slider.value = maxHealth;
+        healthBarCanvas.enabled = false; // Set the canvas to be initially hidden
     }
 
     public void SetHealth(int currentHealth)
     {
         slider.value = currentHealth;
+
+        // Enable the health bar canvas when health drops below 100%
+        if (currentHealth < slider.maxValue)
+        {
+            healthBarCanvas.enabled = true;
+        }
     }
 }
