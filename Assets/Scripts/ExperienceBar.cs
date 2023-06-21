b9c2a79b8c1aa140da67c25804047825f50e7c26
DuzziGit <danteduzzi@live.ca>
2023-06-21 13:52:34 -0400

    Add filters to the background layers

--- Assets/Scripts/ExperienceBar.cs
+++ Assets/Scripts/ExperienceBar.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ExperienceBar : MonoBehaviour
 {
-        public Slider slider;
+    public Slider slider;
 
-     public void setMaxExp ( int playerXp){
+    public void setMaxExp(int playerXp)
+    {
         slider.maxValue = playerXp;
         slider.value = 0;
     }
 
-    public void SetExperience(int playerXp){
-        slider.value = playerXp;
+    public void SetExperience(int playerXp)
+    {
+        slider.value = Mathf.Min(playerXp, slider.maxValue);
     }
-
-
 }
--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -12,10 +12,16 @@ public class Skeleton : Enemy
     public TMP_Text enemyLevel;
     public GameObject CanvasDamageNum;
     public bool isTouchingPlayer = false;
-    private HealthBar healthBar; // Reference to the HealthBar script
-    public Animator animFeedback;   // Declare animFeedback as an Animator
+    private HealthBar healthBar;
+    public Animator animFeedback;
     public Animator animFeedback2;
-    private int maxHealth; // Define the maxHealth variable
+    private int maxHealth;
+
+    // Magic numbers replaced with constants
+    private const float xOffset = 5f;
+    private const float yOffset = 2f;
+    private const float damageDisplayDelay = 0.1f;
+    private const float resetTriggerDelay = 0.2f;
 
     void Start()
     {
@@ -23,75 +29,53 @@ public class Skeleton : Enemy
         enemyLevel.text = level.ToString();
         enemyDamage = level * 5;
 
-      /*  if (level > 0 && level < 10) TMP_Text.color = tutEnemy;
-        else if (level > 10 && level < 20) TMP_Text.color = smallEnemy;
-        else if (level > 20 && level < 30) TMP_Text.color = medEnemy;
-        else if (level > 30 && level < 40) TMP_Text.color = bigEnemy;
-*/
         animator = GetComponent<Animator>();
-
         healthBar = GetComponentInChildren<HealthBar>();
-
-        maxHealth = health; // Set the maxHealth variable to the initial health value
+        maxHealth = health;
 
         if (healthBar != null)
         {
             healthBar.SetMaxHealth(maxHealth);
         }
     }
+
     void Update()
     {
-      //  Debug.Log("max health" + maxHealth);
-     //   Debug.Log("Current Health: " + health);
     }
+
     public void TakeDamage(int damage)
     {
-        health -= damage;
-      // Debug.Log("Damage Taken: " + damage);
+        health = Mathf.Max(0, health - damage);
         StartCoroutine(DamageDisplay(damage));
-        // Damage calculation and other logic
+
         if (healthBar != null)
         {
             healthBar.SetHealth(health);
-       //     Debug.Log("max health" + maxHealth);
-   //         Debug.Log("Current Health: " + health);
         }
 
-        // Trigger the flashing animation
         animator.SetBool("takingDamage", true);
         animFeedback.SetBool("takingDamage", true);
         animFeedback2.SetBool("takingDamage", true);
 
-        //  Debug.Log("taken damage ");
-
-        // Delay the reset of the trigger parameter
         StartCoroutine(ResetTakeDamageTrigger());
-        // audioSource.PlayOneShot(skeletonHitSound, 0.7f);
     }
+
     IEnumerator DamageDisplay(int damage)
     {
-        float xOffset = 5f; 
-        float yOffset = 2f; 
         Vector3 positionOffset = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset + Random.Range(1.0f, 3.0f), transform.position.z);
         GameObject text = Instantiate(CanvasDamageNum, positionOffset, Quaternion.identity);
 
-
         DamageNumController controller = text.GetComponentInChildren<DamageNumController>();
-        
-            controller.SetDamageNum(damage);
-        
-        // Wait a short time before instantiating the next damage number to make them stack
-        yield return new WaitForSeconds(0.1f);
+
+        controller.SetDamageNum(damage);
+        yield return new WaitForSeconds(damageDisplayDelay);
     }
+
     private IEnumerator ResetTakeDamageTrigger()
     {
-        // Wait for the flashing animation to complete
-        yield return new WaitForSeconds(0.2f);
-
-        // Reset the trigger parameter
+        yield return new WaitForSeconds(resetTriggerDelay);
         animator.SetBool("takingDamage", false);
         animFeedback.SetBool("takingDamage", false);
-        animFeedback2.SetBool("takingDamage", false); 
-
+        animFeedback2.SetBool("takingDamage", false);
     }
 }
