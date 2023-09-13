27036690bafe7acac10de31096d5f816fe361d70
DuzziGit <danteduzzi@live.ca>
2023-09-13 14:57:57 -0400

    Controls Only are visable at the start of the scene

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,46 +7,58 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     public int sceneIndex = 0;
-
-    // Reference to the black square Image component
     public Image blackSquare;
-
-    // Position to teleport the player to
     private Vector3 teleportPosition = new Vector3(-103f, 103f, 2f);
 
-   private void OnTriggerStay2D(Collider2D collision)
-{
-    Debug.Log("Object entered the portal's trigger.");
-
-    if (collision.CompareTag("Player"))
+    private void Start()
     {
-        Debug.Log("Player is inside the portal's trigger.");
+        // Add the fade-in as an event when a new scene is loaded
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-        if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.UpArrow))
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Fade back in when a new scene is loaded
+        StartCoroutine(FadeBlackOutSquare(false, 1));
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Object entered the portal's trigger.");
+
+        if (collision.CompareTag("Player"))
         {
-            Debug.Log("Attempting to fade out.");
-            StartCoroutine(FadeAndLoadScene(collision));
+            Debug.Log("Player is inside the portal's trigger.");
+
+            if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Debug.Log("Attempting to fade out.");
+                StartCoroutine(FadeAndLoadScene(collision));
+            }
         }
     }
-}
 
-
-  IEnumerator FadeAndLoadScene(Collider2D playerCollider)
+    IEnumerator FadeAndLoadScene(Collider2D playerCollider)
 {
     yield return StartCoroutine(FadeBlackOutSquare(true, 1));
 
-    // Teleport the player after fade-out
+    // After fade-out is complete, wait for the specified time
+    yield return new WaitForSeconds(1f);
+
+    // Teleport the player
     RogueSkillController rogue = playerCollider.GetComponent<RogueSkillController>();
     if (rogue != null)
     {
         rogue.transform.position = teleportPosition;
     }
 
+    // Then, load the new scene. Once the new scene is loaded, 
+    // the OnSceneLoaded event will automatically trigger the fade-in.
     Debug.Log("Attempting to load scene.");
     SceneManager.LoadScene(sceneIndex);
 }
 
-    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 1)
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 2)
     {
         Debug.Log("Fade");
         Color objectColor = blackSquare.color;
@@ -57,7 +69,6 @@ public class Portal : MonoBehaviour
             while (blackSquare.color.a < 1)
             {
                 fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
-
                 objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
                 blackSquare.color = objectColor;
                 yield return null;
@@ -68,11 +79,16 @@ public class Portal : MonoBehaviour
             while (blackSquare.color.a > 0)
             {
                 fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
-
                 objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
                 blackSquare.color = objectColor;
                 yield return null;
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        // Important to unsubscribe the event to prevent memory leaks
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
